641b1c751dbc24616ea320e805ffe12aad65872a
Wei Sun <waysun@amazon.com>
2019-07-31 18:03:58 +0800

    nil

--- leetcode/rust/remove_all_adjacent_duplicates_in_string/src/main.rs
+++ leetcode/rust/remove_all_adjacent_duplicates_in_string/src/main.rs
@@ -1,12 +1,21 @@
+// Accepted
 // @lc id=1047 lang=rust
 // problem: remove_all_adjacent_duplicates_in_string
 struct Solution {}
 impl Solution {
     pub fn remove_duplicates(s: String) -> String {
-        return "".to_owned();
+        let mut stack = vec![];
+        for c in s.chars() {
+            if stack.last().unwrap_or(&'0') == &c {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        stack.into_iter().collect::<String>()
     }
 }
 
 fn main() {
-    println!("{:?}", Solution::remove_duplicates("abbca".to_string()));
+    println!("{:?}", Solution::remove_duplicates("abbaca".to_string()));
 }
