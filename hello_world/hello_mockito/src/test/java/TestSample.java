d78c9b34c4b18feefc42ef501948ac72ae9089aa
sunway <sunwayforever@gmail.com>
2019-08-02 23:42:01 +0800

    nil

--- hello_world/hello_mockito/src/test/java/TestSample.java
+++ hello_world/hello_mockito/src/test/java/TestSample.java
@@ -1,5 +1,8 @@
 // 2019-07-27 20:40
 import static org.mockito.Mockito.*;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.MockitoAnnotations;
 import java.util.List;
 import java.util.ArrayList;
 import org.junit.Test;
@@ -24,11 +27,21 @@ public class TestSample {
         assertEquals(x.get(0).intValue(), 1);
     }
 
+    @Mock
+    ArrayList<Integer> mMockList;
+
+    @Spy
+    ArrayList<Integer> mSpyList;
+
     @Test
     public void testAnnotation() {
-        @Mock
-        ArrayList<Integer> x;
-        when (x.size()).thenReturn(1);
-        assertEquals(x.size(), 1);
+        MockitoAnnotations.initMocks(this);
+        when (mMockList.size()).thenReturn(1);
+        assertEquals(mMockList.size(), 1);
+
+        doReturn(2).when(mSpyList).size();
+        mSpyList.add(1);
+        assertEquals(mSpyList.size(), 2);
+        assertEquals(mSpyList.get(0).intValue(), 1);
     }
 }
