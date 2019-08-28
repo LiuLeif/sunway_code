8d1169b022f97e13701eb71ff7aea9e912a3f266
Wei Sun <waysun@amazon.com>
2019-08-28 11:23:30 +0800

    nil

--- hello_world/hello_linked_list/src/second.rs
+++ hello_world/hello_linked_list/src/second.rs
@@ -1,4 +1,5 @@
 // 2019-08-27 13:24
+#[derive(Default)]
 pub struct List<T> {
     pub head: Link<T>,
 }
@@ -11,10 +12,6 @@ pub struct Node<T> {
 }
 
 impl<T> List<T> {
-    pub fn new() -> Self {
-        List { head: None }
-    }
-
     pub fn push(&mut self, elem: T) {
         let new_node = Box::new(Node {
             elem,
@@ -42,9 +39,48 @@ impl<T> List<T> {
     }
 }
 
+// into iterator
+pub struct ListIntoIterator<T>(List<T>);
+
+impl<T> std::iter::IntoIterator for List<T> {
+    type Item = T;
+    type IntoIter = ListIntoIterator<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        ListIntoIterator(self)
+    }
+}
+
+impl<T> Iterator for ListIntoIterator<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<T> {
+        self.0.pop()
+    }
+}
+
+// iter
+impl<T> List<T> {
+    pub fn iter(&mut self) -> ListIter<T> {
+        ListIter {
+            next: self.head.as_ref()
+        }
+    }
+}
+pub struct ListIter<'a, T> {
+    next: Option<&'a Box<Node<T>>>,
+}
+
+impl<'a, T> std::iter::Iterator for ListIter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<&'a T> {
+        self.next.map( |node| {
+
+        }})
+    }
+}
 #[test]
 fn test_push_and_pop() {
-    let mut list = List::new();
+    let mut list = List::default();
     list.push(2);
     assert_eq!(list.pop().unwrap(), 2);
     assert_eq!(list.pop(), None);
@@ -52,7 +88,7 @@ fn test_push_and_pop() {
 
 #[test]
 fn test_peek() {
-    let mut list = List::new();
+    let mut list = List::default();
     list.push(2);
     list.peek_mut().map(|value| {
         *value = 3;
@@ -60,4 +96,17 @@ fn test_peek() {
     assert_eq!(list.peek().unwrap(), &3);
     list.pop();
     assert!(list.peek().is_none());
+
+    let mut list2 = List::default();
+    list2.push(1);
+    list2.push(2);
+    assert_eq!(list2.into_iter().collect::<Vec<_>>(), vec![2, 1]);
+}
+
+#[test]
+fn test_into_iterator() {
+    let mut list2 = List::default();
+    list2.push(1);
+    list2.push(2);
+    assert_eq!(list2.into_iter().collect::<Vec<_>>(), vec![2, 1]);
 }
