e424f422edb03d0d393aa84385461ff7ef1497ee
Wei Sun <waysun@amazon.com>
2019-09-02 19:02:39 +0800

    nil

---
+++ makaira/class_parser/src/class_file.rs
@@ -0,0 +1,205 @@
+#[derive(Default, Debug)]
+pub struct ClassFile {
+    pub magic: u32,
+    pub minor_vesion: u16,
+    pub major_vesion: u16,
+    pub constant_pool_count: u16,
+    pub constant_pool: Vec<ConstantInfo>,
+    pub access_flags: u16,
+    pub this_class: u16,
+    pub super_class: u16,
+    pub interface_count: u16,
+    pub interfaces: Vec<u16>,
+    pub fields_count: u16,
+    pub fields: Vec<FieldInfo>,
+    pub methods_count: u16,
+    pub methods: Vec<MethodInfo>,
+    pub attributes_count: u16,
+    pub attributes: Vec<AttributeInfo>,
+}
+
+impl ClassFile {
+    pub fn resolve(&mut self) {}
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum ConstantType {
+    Class = 7,
+    Fieldref = 9,
+    Methodref = 10,
+    InterfaceMethodref = 11,
+    String = 8,
+    Integer = 3,
+    Float = 4,
+    Long = 5,
+    Double = 6,
+    NameAndType = 12,
+    Utf8 = 1,
+}
+
+impl ConstantType {
+    pub fn from_u8(v: u8) -> Option<Self> {
+        match v {
+            1 => Some(Self::Utf8),
+            3 => Some(Self::Integer),
+            4 => Some(Self::Float),
+            5 => Some(Self::Long),
+            6 => Some(Self::Double),
+            7 => Some(Self::Class),
+            8 => Some(Self::String),
+            9 => Some(Self::Fieldref),
+            10 => Some(Self::Methodref),
+            11 => Some(Self::InterfaceMethodref),
+            12 => Some(Self::NameAndType),
+            _ => None,
+        }
+    }
+}
+
+impl Default for ConstantType {
+    fn default() -> Self {
+        Self::Class
+    }
+}
+
+impl Default for ConstantInfo {
+    fn default() -> Self {
+        Self::ClassInfo {
+            tag: ConstantType::Class,
+            name_index: 0,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum ConstantInfo {
+    ClassInfo {
+        tag: ConstantType,
+        name_index: u16,
+    },
+    FieldrefInfo {
+        tag: ConstantType,
+        class_index: u16,
+        name_and_type_index: u16,
+    },
+    MethodrefInfo {
+        tag: ConstantType,
+        class_index: u16,
+        name_and_type_index: u16,
+    },
+    InterfaceMethodrefInfo {
+        tag: ConstantType,
+        class_index: u16,
+        name_and_type_index: u16,
+    },
+    StringInfo {
+        tag: ConstantType,
+        string_index: u16,
+    },
+    IntegerInfo {
+        tag: ConstantType,
+        bytes: u32,
+    },
+    FloatInfo {
+        tag: ConstantType,
+        bytes: u32,
+    },
+    LongInfo {
+        tag: ConstantType,
+        high_bytes: u32,
+        low_bytes: u32,
+    },
+    DoubleInfo {
+        tag: ConstantType,
+        high_bytes: u32,
+        low_bytes: u32,
+    },
+    NameAndTypeInfo {
+        tag: ConstantType,
+        name_index: u16,
+        descriptor_index: u16,
+    },
+    Utf8Info {
+        tag: ConstantType,
+        length: u16,
+        bytes: Vec<u8>,
+    },
+}
+
+#[derive(Default, Debug)]
+pub struct FieldInfo {
+    pub access_flags: u16,
+    pub name_index: u16,
+    pub descriptor_index: u16,
+    pub attributes_count: u16,
+    pub attributes: Vec<AttributeInfo>,
+}
+
+#[derive(Default, Debug)]
+pub struct MethodInfo {
+    pub access_flags: u16,
+    pub name_index: u16,
+    pub descriptor_index: u16,
+    pub attributes_count: u16,
+    pub attributes: Vec<AttributeInfo>,
+}
+
+#[derive(Debug)]
+pub enum AttributeInfo {
+    RawInfo {
+        attribute_name_index: u16,
+        attribute_length: u32,
+        raw_data: Vec<u8>,
+    },
+    ConstantValue {
+        attribute_name_index: u16,
+        attribute_length: u32,
+        constantvalue_index: u16,
+    },
+    Code {
+        attribute_name_index: u16,
+        attribute_length: u32,
+        max_stack: u16,
+        max_locals: u16,
+        code_length: u32,
+        code: Vec<u8>,
+        exception_table_length: u16,
+        exception_table: Vec<ExceptionTableEntry>,
+        attributes_count: u16,
+        attributes: Vec<AttributeInfo>,
+    },
+}
+
+impl Default for AttributeInfo {
+    fn default() -> Self {
+        Self::ConstantValue {
+            attribute_name_index: 0,
+            attribute_length: 0,
+            constantvalue_index: 0,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct ExceptionTableEntry {
+    start_pc: u16,
+    end_pc: u16,
+    handler_pc: u16,
+    catch_type: u16,
+}
+
+pub enum AccessFlags {
+    ACC_PUBLIC = 0x1,
+    ACC_PRIVATE = 0x2,
+    ACC_PROTECTED = 0x4,
+    ACC_STATIC = 0x8,
+    ACC_FINAL = 0x10,
+    ACC_SUPER = 0x20,
+    ACC_VOATIVATE = 0x40,
+    ACC_TRANSIENT = 0x80,
+    ACC_INTERFACE = 0x200,
+    ACC_ABSTRACT = 0x400,
+    ACC_SYNTHETIC = 0x1000,
+    ACC_ANNOTATION = 0x2000,
+    ACC_ENUM = 0x4000,
+}
---
+++ makaira/class_parser/src/parser.rs
@@ -0,0 +1,193 @@
+use nom::bytes::complete::*;
+use nom::character::complete::*;
+use nom::number::complete::*;
+// use nom::bits::complete::*;
+
+use nom::branch::*;
+use nom::combinator::*;
+use nom::error::*;
+use nom::multi::*;
+use nom::sequence::*;
+use nom::*;
+
+use crate::class_file::*;
+
+fn parse_constant_info(input: &[u8]) -> IResult<&[u8], ConstantInfo> {
+    let (input, constant_type) = map_opt(be_u8, |t| ConstantType::from_u8(t))(input)?;
+    match constant_type {
+        ConstantType::Class => map(be_u16, |name_index| ConstantInfo::ClassInfo {
+            tag: constant_type,
+            name_index,
+        })(input),
+        ConstantType::Fieldref => map(
+            pair(be_u16, be_u16),
+            |(class_index, name_and_type_index)| ConstantInfo::FieldrefInfo {
+                tag: constant_type,
+                class_index,
+                name_and_type_index,
+            },
+        )(input),
+        ConstantType::Methodref => map(
+            pair(be_u16, be_u16),
+            |(class_index, name_and_type_index)| ConstantInfo::MethodrefInfo {
+                tag: constant_type,
+                class_index,
+                name_and_type_index,
+            },
+        )(input),
+        ConstantType::InterfaceMethodref => map(
+            pair(be_u16, be_u16),
+            |(class_index, name_and_type_index)| ConstantInfo::InterfaceMethodrefInfo {
+                tag: constant_type,
+                class_index,
+                name_and_type_index,
+            },
+        )(input),
+        ConstantType::String => map(be_u16, |string_index| ConstantInfo::StringInfo {
+            tag: constant_type,
+            string_index,
+        })(input),
+        ConstantType::Integer => map(be_u32, |bytes| ConstantInfo::IntegerInfo {
+            tag: constant_type,
+            bytes,
+        })(input),
+        ConstantType::Float => map(be_u32, |bytes| ConstantInfo::FloatInfo {
+            tag: constant_type,
+            bytes,
+        })(input),
+        ConstantType::Long => map(pair(be_u32, be_u32), |(high_bytes, low_bytes)| {
+            ConstantInfo::LongInfo {
+                tag: constant_type,
+                high_bytes,
+                low_bytes,
+            }
+        })(input),
+        ConstantType::Double => map(pair(be_u32, be_u32), |(high_bytes, low_bytes)| {
+            ConstantInfo::DoubleInfo {
+                tag: constant_type,
+                high_bytes,
+                low_bytes,
+            }
+        })(input),
+        ConstantType::NameAndType => map(pair(be_u16, be_u16), |(name_index, descriptor_index)| {
+            ConstantInfo::NameAndTypeInfo {
+                tag: constant_type,
+                name_index,
+                descriptor_index,
+            }
+        })(input),
+        ConstantType::Utf8 => {
+            let (input, length) = be_u16(input)?;
+            let (input, bytes) = take(length)(input)?;
+            Ok((
+                input,
+                ConstantInfo::Utf8Info {
+                    tag: constant_type,
+                    length,
+                    bytes: bytes.to_owned(),
+                },
+            ))
+        } // _ => Err(Err::Error((input, ErrorKind::Tag))),
+    }
+}
+
+fn parse_constant_pool(input: &[u8]) -> IResult<&[u8], (u16, Vec<ConstantInfo>)> {
+    pair(be_u16, many0(parse_constant_info))(input)
+}
+
+fn parse_interface(input: &[u8]) -> IResult<&[u8], (u16, Vec<u16>)> {
+    let (input, interface_count) = be_u16(input)?;
+    let (input, interfaces) = count(be_u16, interface_count as usize)(input)?;
+    Ok((input, (interface_count, interfaces)))
+}
+
+fn parse_attributes(input: &[u8]) -> IResult<&[u8], (u16, Vec<AttributeInfo>)> {
+    let (input, attributes_count) = be_u16(input)?;
+    let (input, attribute_info) = count(
+        map(
+            pair(be_u16, length_data(be_u32)),
+            |(attribute_name_index, raw_data)| AttributeInfo::RawInfo {
+                attribute_name_index,
+                attribute_length: raw_data.len() as u32,
+                raw_data: raw_data.to_vec(),
+            },
+        ),
+        attributes_count as usize,
+    )(input)?;
+    Ok((input, (attributes_count, attribute_info)))
+}
+
+fn parse_fields(input: &[u8]) -> IResult<&[u8], (u16, Vec<FieldInfo>)> {
+    let (input, fields_count) = be_u16(input)?;
+    let (input, fields) = count(
+        map(
+            tuple((be_u16, be_u16, be_u16, parse_attributes)),
+            |(access_flags, name_index, descriptor_index, (attributes_count, attributes))| {
+                FieldInfo {
+                    access_flags,
+                    name_index,
+                    descriptor_index,
+                    attributes_count,
+                    attributes,
+                }
+            },
+        ),
+        fields_count as usize,
+    )(input)?;
+    Ok((input, (fields_count, fields)))
+}
+
+fn parse_methods(input: &[u8]) -> IResult<&[u8], (u16, Vec<MethodInfo>)> {
+    let (input, methods_count) = be_u16(input)?;
+    let (input, methods) = count(
+        map(
+            tuple((be_u16, be_u16, be_u16, parse_attributes)),
+            |(access_flags, name_index, descriptor_index, (attributes_count, attributes))| {
+                MethodInfo {
+                    access_flags,
+                    name_index,
+                    descriptor_index,
+                    attributes_count,
+                    attributes,
+                }
+            },
+        ),
+        methods_count as usize,
+    )(input)?;
+    Ok((input, (methods_count, methods)))
+}
+
+pub fn parse_class(input: &[u8]) -> IResult<&[u8], ClassFile> {
+    let mut ret = ClassFile::default();
+    let (input, (magic, minor_vesion, major_vesion)) = tuple((be_u32, be_u16, be_u16))(input)?;
+    ret.magic = magic;
+    ret.minor_vesion = minor_vesion;
+    ret.major_vesion = major_vesion;
+
+    let (input, (constant_pool_count, constant_pool)) = parse_constant_pool(input)?;
+    ret.constant_pool_count = constant_pool_count;
+    ret.constant_pool = constant_pool;
+
+    let (input, (access_flags, this_class, super_class)) = tuple((be_u16, be_u16, be_u16))(input)?;
+    ret.access_flags = access_flags;
+    ret.this_class = this_class;
+    ret.super_class = super_class;
+
+    let (input, (interface_count, interfaces)) = parse_interface(input)?;
+    ret.interface_count = interface_count;
+    ret.interfaces = interfaces;
+
+    let (input, (fields_count, fields)) = parse_fields(input)?;
+    ret.fields_count = fields_count;
+    ret.fields = fields;
+
+    let (input, (methods_count, methods)) = parse_methods(input)?;
+    ret.methods_count = methods_count;
+    ret.methods = methods;
+
+    let (input, (attributes_count, attributes)) = parse_attributes(input)?;
+    ret.attributes_count = attributes_count;
+    ret.attributes = attributes;
+
+    Ok((input, ret))
+}
