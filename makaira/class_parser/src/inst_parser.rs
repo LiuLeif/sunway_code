f899c8459faafb9bb64edb48bb2a75f50e73a04c
Wei Sun <waysun@amazon.com>
2019-09-05 19:35:28 +0800

    nil

--- makaira/class_parser/src/inst_parser.rs
+++ makaira/class_parser/src/inst_parser.rs
@@ -81,7 +81,7 @@ pub enum MakairaInst {
     FCONST_1,
     FCONST_2,
     FDIV,
-    FLOAD,
+    FLOAD(u8),
     FLOAD_0,
     FLOAD_1,
     FLOAD_2,
@@ -90,16 +90,16 @@ pub enum MakairaInst {
     FNEG,
     FREM,
     FRETURN,
-    FSTORE,
+    FSTORE(u8),
     FSTORE_0,
     FSTORE_1,
     FSTORE_2,
     FSTORE_3,
     FSUB,
-    GETFIELD,
-    GETSTATIC,
-    GOTO,
-    GOTO_W,
+    GETFIELD(u16),
+    GETSTATIC(u16),
+    GOTO(u16),
+    GOTO_W(u32),
     I2B,
     I2C,
     I2D,
@@ -118,24 +118,24 @@ pub enum MakairaInst {
     ICONST_4,
     ICONST_5,
     IDIV,
-    IF_ACMPEQ,
-    IF_ACMPNE,
-    IF_ICMPEQ,
-    IF_ICMPGE,
-    IF_ICMPGT,
-    IF_ICMPLE,
-    IF_ICMPLT,
-    IF_ICMPNE,
-    IFEQ,
-    IFGE,
-    IFGT,
-    IFLE,
-    IFLT,
-    IFNE,
-    IFNONNULL,
-    IFNULL,
-    IINC,
-    ILOAD,
+    IF_ACMPEQ(u16),
+    IF_ACMPNE(u16),
+    IF_ICMPEQ(u16),
+    IF_ICMPGE(u16),
+    IF_ICMPGT(u16),
+    IF_ICMPLE(u16),
+    IF_ICMPLT(u16),
+    IF_ICMPNE(u16),
+    IFEQ(u16),
+    IFGE(u16),
+    IFGT(u16),
+    IFLE(u16),
+    IFLT(u16),
+    IFNE(u16),
+    IFNONNULL(u16),
+    IFNULL(u16),
+    IINC(u16),
+    ILOAD(u8),
     ILOAD_0,
     ILOAD_1,
     ILOAD_2,
@@ -144,18 +144,17 @@ pub enum MakairaInst {
     IMPDEP2,
     IMUL,
     INEG,
-    INSTANCEOF,
-    INVOKEDYNAMIC,
-    INVOKEINTERFACE,
+    INSTANCEOF(u16),
+    INVOKEINTERFACE(u16, u8),
     INVOKESPECIAL(u16),
-    INVOKESTATIC,
-    INVOKEVIRTUAL,
+    INVOKESTATIC(u16),
+    INVOKEVIRTUAL(u16),
     IOR,
     IREM,
     IRETURN,
     ISHL,
     ISHR,
-    ISTORE,
+    ISTORE(u8),
     ISTORE_0,
     ISTORE_1,
     ISTORE_2,
@@ -163,8 +162,8 @@ pub enum MakairaInst {
     ISUB,
     IUSHR,
     IXOR,
-    JSR,
-    JSR_W,
+    JSR(u16),
+    JSR_W(u32),
     L2D,
     L2F,
     L2I,
@@ -175,11 +174,11 @@ pub enum MakairaInst {
     LCMP,
     LCONST_0,
     LCONST_1,
-    LDC,
-    LDC_W,
-    LDC2_W,
+    LDC(u8),
+    LDC_W(u16),
+    LDC2_W(u16),
     LDIV,
-    LLOAD,
+    LLOAD(u8),
     LLOAD_0,
     LLOAD_1,
     LLOAD_2,
@@ -192,7 +191,7 @@ pub enum MakairaInst {
     LRETURN,
     LSHL,
     LSHR,
-    LSTORE,
+    LSTORE(u8),
     LSTORE_0,
     LSTORE_1,
     LSTORE_2,
@@ -202,19 +201,19 @@ pub enum MakairaInst {
     LXOR,
     MONITORENTER,
     MONITOREXIT,
-    MULTIANEWARRAY,
-    NEW,
-    NEWARRAY,
+    MULTIANEWARRAY(u16, u8),
+    NEW(u16),
+    NEWARRAY(u8),
     NOP,
     POP,
     POP2,
-    PUTFIELD,
-    PUTSTATIC,
-    RET,
+    PUTFIELD(u16),
+    PUTSTATIC(u16),
+    RET(u8),
     RETURN,
     SALOAD,
     SASTORE,
-    SIPUSH,
+    SIPUSH(u16),
     SWAP,
     TABLESWITCH,
     WIDE,
@@ -231,27 +230,12 @@ fn parse_inst(input: &[u8]) -> IResult<&[u8], MakairaInst> {
             0x32 => AALOAD,
             0x53 => AASTORE,
             0x1 => ACONST_NULL,
-            0x19 => {
-                let (tmp, index) = be_u8(input)?;
-                input = tmp;
-                ALOAD(index)
-            }
             0x2a => ALOAD_0,
             0x2b => ALOAD_1,
             0x2c => ALOAD_2,
             0x2d => ALOAD_3,
-            0xbd => {
-                let (tmp, (hi, lo)) = pair(be_u8, be_u8)(input)?;
-                input = tmp;
-                ANEWARRAY(((hi as u16) << 8) + lo as u16)
-            }
             0xb0 => ARETURN,
             0xbe => ARRAYLENGTH,
-            0x3a => {
-                let (tmp, index) = be_u8(input)?;
-                input = tmp;
-                ASTORE(index)
-            }
             0x4b => ASTORE_0,
             0x4c => ASTORE_1,
             0x4d => ASTORE_2,
@@ -259,18 +243,8 @@ fn parse_inst(input: &[u8]) -> IResult<&[u8], MakairaInst> {
             0xbf => ATHROW,
             0x33 => BALOAD,
             0x54 => BASTORE,
-            0x10 => {
-                let (tmp, byte) = be_u8(input)?;
-                input = tmp;
-                BIPUSH(byte)
-            },
             0x34 => CALOAD,
             0x55 => CASTORE,
-            0xc0 => {
-                let (tmp, (hi, lo)) = pair(be_u8, be_u8)(input)?;
-                input = tmp;
-                CHECKCAST(((hi as u16) << 8) + lo as u16)
-            },
             0x90 => D2F,
             0x8e => D2I,
             0x8f => D2L,
@@ -282,11 +256,6 @@ fn parse_inst(input: &[u8]) -> IResult<&[u8], MakairaInst> {
             0x0e => DCONST_0,
             0x0f => DCONST_1,
             0x6f => DDIV,
-            0x18 => {
-                let (tmp, index) = be_u8(input)?;
-                input = tmp;
-                DLOAD(index)
-            },
             0x26 => DLOAD_0,
             0x27 => DLOAD_1,
             0x28 => DLOAD_2,
@@ -295,11 +264,6 @@ fn parse_inst(input: &[u8]) -> IResult<&[u8], MakairaInst> {
             0x77 => DNEG,
             0x73 => DREM,
             0xaf => DRETURN,
-            0x39 => {
-                let (tmp, index) = be_u8(input)?;
-                input = tmp;
-                DSTORE(index)
-            },
             0x47 => DSTORE_0,
             0x48 => DSTORE_1,
             0x49 => DSTORE_2,
@@ -322,12 +286,182 @@ fn parse_inst(input: &[u8]) -> IResult<&[u8], MakairaInst> {
             0x0b => FCONST_0,
             0x0c => FCONST_1,
             0x0d => FCONST_2,
-            0xb7 => {
-                let (tmp, (hi, lo)) = pair(be_u8, be_u8)(input)?;
-                input = tmp;
-                INVOKESPECIAL(((hi as u16) << 8) + lo as u16)
-            }
+            0x6e => FDIV,
+            0x22 => FLOAD_0,
+            0x23 => FLOAD_1,
+            0x24 => FLOAD_2,
+            0x25 => FLOAD_3,
+            0x6a => FMUL,
+            0x76 => FNEG,
+            0x72 => FREM,
+            0xae => FRETURN,
+            0x43 => FSTORE_0,
+            0x44 => FSTORE_1,
+            0x45 => FSTORE_2,
+            0x46 => FSTORE_3,
+            0x66 => FSUB,
+            0x91 => I2B,
+            0x92 => I2C,
+            0x87 => I2D,
+            0x86 => I2F,
+            0x85 => I2L,
+            0x93 => I2S,
+            0x60 => IADD,
+            0x2e => IALOAD,
+            0x7e => IAND,
+            0x4f => IASTORE,
+            0x02 => ICONST_M1,
+            0x03 => ICONST_0,
+            0x04 => ICONST_1,
+            0x05 => ICONST_2,
+            0x06 => ICONST_3,
+            0x07 => ICONST_4,
+            0x08 => ICONST_5,
+            0x6c => IDIV,
+            0x1a => ILOAD_0,
+            0x1b => ILOAD_1,
+            0x1c => ILOAD_2,
+            0x1d => ILOAD_3,
+            0xfe => IMPDEP1,
+            0xff => IMPDEP2,
+            0x68 => IMUL,
+            0x74 => INEG,
+            0x80 => IOR,
+            0x70 => IREM,
+            0xac => IRETURN,
+            0x78 => ISHL,
+            0x7a => ISHR,
+            0x3b => ISTORE_0,
+            0x3c => ISTORE_1,
+            0x3d => ISTORE_2,
+            0x3e => ISTORE_3,
+            0x64 => ISUB,
+            0x7c => IUSHR,
+            0x82 => IXOR,
+            0x8a => L2D,
+            0x89 => L2F,
+            0x88 => L2I,
+            0x61 => LADD,
+            0x2f => LALOAD,
+            0x7f => LAND,
+            0x50 => LASTORE,
+            0x94 => LCMP,
+            0x09 => LCONST_0,
+            0x0a => LCONST_1,
+            0x6d => LDIV,
+            0x1e => LLOAD_0,
+            0x1f => LLOAD_1,
+            0x20 => LLOAD_2,
+            0x21 => LLOAD_3,
+            0x69 => LMUL,
+            0x75 => LNEG,
+            0x81 => LOR,
+            0x71 => LREM,
+            0xad => LRETURN,
+            0x79 => LSHL,
+            0x7b => LSHR,
+            0x3f => LSTORE_0,
+            0x40 => LSTORE_1,
+            0x41 => LSTORE_2,
+            0x42 => LSTORE_3,
+            0x65 => LSUB,
+            0x7d => LUSHR,
+            0x83 => LXOR,
+            0xc2 => MONITORENTER,
+            0xc3 => MONITOREXIT,
+            0x00 => NOP,
+            0x57 => POP,
+            0x58 => POP2,
             0xb1 => RETURN,
+            0x35 => SALOAD,
+            0x56 => SASTORE,
+            0x5f => SWAP,
+            0x19 | 0x3a | 0x10 | 0x18 | 0x39 | 0x17 | 0x38 | 0x15 | 0x36 | 0x12 | 0x16 | 0x37
+            | 0xbc | 0xa9 => {
+                let (tmp, index) = be_u8(input)?;
+                input = tmp;
+                match opcode {
+                    0x19 => ALOAD(index),
+                    0x3a => ASTORE(index),
+                    0x10 => BIPUSH(index),
+                    0x18 => DLOAD(index),
+                    0x39 => DSTORE(index),
+                    0x17 => FLOAD(index),
+                    0x38 => FSTORE(index),
+                    0x15 => ILOAD(index),
+                    0x36 => ISTORE(index),
+                    0x12 => LDC(index),
+                    0x16 => LLOAD(index),
+                    0x37 => LSTORE(index),
+                    0xbc => NEWARRAY(index),
+                    0xa9 => RET(index),
+                    _ => panic!(),
+                }
+            }
+            0x64 | 0xb2 | 0xa7 | 0xb7 | 0xbd | 0xc0 | 0xa5 | 0xa6 | 0x9f | 0xa2 | 0xa3 | 0xa4
+            | 0xa1 | 0xa0 | 0x99 | 0x9c | 0x9d | 0x9e | 0x9b | 0x9a | 0xc7 | 0xc6 | 0x84 | 0xc1
+            | 0xb7 | 0xb8 | 0xb6 | 0xa8 | 0x13 | 0x14 | 0xbb | 0xb5 | 0xb3 | 0x11 => {
+                let (tmp, value) = be_u16(input)?;
+                input = tmp;
+                match opcode {
+                    0xbd => ANEWARRAY(value),
+                    0xc0 => CHECKCAST(value),
+                    0x64 => GETFIELD(value),
+                    0xb2 => GETSTATIC(value),
+                    0xa7 => GOTO(value),
+                    0xa5 => IF_ACMPEQ(value),
+                    0xa6 => IF_ACMPNE(value),
+                    0x9f => IF_ICMPEQ(value),
+                    0xa2 => IF_ICMPGE(value),
+                    0xa3 => IF_ICMPGT(value),
+                    0xa4 => IF_ICMPLE(value),
+                    0xa1 => IF_ICMPLT(value),
+                    0xa0 => IF_ICMPNE(value),
+                    0x99 => IFEQ(value),
+                    0x9c => IFGE(value),
+                    0x9d => IFGT(value),
+                    0x9e => IFLE(value),
+                    0x9b => IFLT(value),
+                    0x9a => IFNE(value),
+                    0xc7 => IFNONNULL(value),
+                    0xc6 => IFNULL(value),
+                    0x84 => IINC(value),
+                    0xc1 => INSTANCEOF(value),
+                    0xb7 => INVOKESPECIAL(value),
+                    0xb8 => INVOKESTATIC(value),
+                    0xb6 => INVOKEVIRTUAL(value),
+                    0xa8 => JSR(value),
+                    0x13 => LDC_W(value),
+                    0x14 => LDC2_W(value),
+                    0xbb => NEW(value),
+                    0xb5 => PUTFIELD(value),
+                    0xb3 => PUTSTATIC(value),
+                    0x11 => SIPUSH(value),
+                    _ => panic!(),
+                }
+            }
+
+            0xc5 => {
+                let (tmp, (value, dimen)) = pair(be_u16, be_u8)(input)?;
+                input = tmp;
+                MULTIANEWARRAY(value, dimen)
+            }
+
+            0xc8 | 0xa8 => {
+                let (tmp, value) = be_u32(input)?;
+                input = tmp;
+                match opcode {
+                    0xc8 => GOTO_W(value),
+                    0xa8 => JSR_W(value),
+                    _ => panic!(),
+                }
+            }
+
+            0xb9 => {
+                let (tmp, (value, count, _)) = tuple((be_u16, be_u8, be_u8))(input)?;
+                INVOKEINTERFACE(value, count)
+            }
+
             // _ => Err(Err::Error((input, ErrorKind::Tag))),
             _ => panic!("unknown inst"),
         }
