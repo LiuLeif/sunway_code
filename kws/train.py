2ff98899e347cacf901a5e127288fee802296925
sunway <sunwayforever@gmail.com>
2020-08-25 13:15:04 +0800

    nil

--- kws/train.py
+++ kws/train.py
@@ -3,12 +3,14 @@
 # 2020-08-24 15:49
 import tensorflow as tf
 import numpy as np
+import os
 
 from tensorflow import keras
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras import layers, losses, metrics, optimizers, models
 
-# from input_data import load_data
+
+# tf.config.set_visible_devices([], "GPU")
 
 x_train = np.load("./temp/train_x.npy")
 y_train = np.load("./temp/train_y.npy")
@@ -20,26 +22,33 @@ x_test = np.squeeze(x_test)
 
 tf.keras.backend.clear_session()
 
-inputs = keras.Input(shape=(49, 10))
-outputs = tf.expand_dims(inputs, axis=-1)
+MODEL_PATH = "./temp/my"
+if os.path.exists(MODEL_PATH):
+    model = keras.models.load_model(MODEL_PATH)
+else:
+    inputs = keras.Input(shape=(49, 10))
+    outputs = tf.expand_dims(inputs, axis=-1)
 
-outputs = layers.Conv2D(filters=64, kernel_size=[10, 4], strides=[1, 1])(outputs)
-outputs = layers.BatchNormalization()(outputs)
-outputs = layers.ReLU()(outputs)
+    outputs = layers.Conv2D(filters=64, kernel_size=[10, 4], strides=[1, 1])(outputs)
+    outputs = layers.BatchNormalization()(outputs)
+    outputs = layers.ReLU()(outputs)
+    outputs = layers.Dropout(0.1)(outputs)
 
-outputs = layers.Conv2D(filters=48, kernel_size=[10, 4], strides=[2, 1])(outputs)
-outputs = layers.BatchNormalization()(outputs)
-outputs = layers.ReLU()(outputs)
+    outputs = layers.Conv2D(filters=48, kernel_size=[10, 4], strides=[2, 1])(outputs)
+    outputs = layers.BatchNormalization()(outputs)
+    outputs = layers.ReLU()(outputs)
+    outputs = layers.Dropout(0.1)(outputs)
 
-outputs = layers.Flatten()(outputs)
-outputs = layers.Dense(16, activation="relu")(outputs)
-outputs = layers.Dense(128, activation="relu")(outputs)
-outputs = layers.Dense(12, activation="softmax")(outputs)
+    outputs = layers.Flatten()(outputs)
+    outputs = layers.Dense(16, activation="relu")(outputs)
+    outputs = layers.Dense(128, activation="relu")(outputs)
+    outputs = layers.Dense(12, activation="softmax")(outputs)
 
-model = keras.Model(inputs, outputs)
+    model = keras.Model(inputs, outputs)
+
 model.compile(
     optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
 )
-model.fit(x_train, y_train, batch_size=200, epochs=10, verbose=1)
+model.fit(x_train, y_train, batch_size=200, epochs=50, verbose=1)
 model.evaluate(x_test, y_test, verbose=2)
-model.save("./temp/my")
+model.save(MODEL_PATH)
