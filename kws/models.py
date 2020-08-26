b4cb6b8f97348969149735a81e1b84065e8696bb
sunway <sunwayforever@gmail.com>
2020-08-26 16:01:10 +0800

    nil

---
+++ kws/models.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# 2020-08-26 09:52
+import tensorflow as tf
+import numpy as np
+
+from tensorflow import keras
+from tensorflow.keras import layers, losses, metrics, optimizers, models
+
+
+def cnn():
+    inputs = keras.Input(shape=(49, 10))
+    outputs = tf.expand_dims(inputs, axis=-1)
+
+    outputs = layers.Conv2D(filters=64, kernel_size=[10, 4], strides=[1, 1])(outputs)
+    outputs = layers.BatchNormalization()(outputs)
+    outputs = layers.ReLU()(outputs)
+    outputs = layers.Dropout(0.1)(outputs)
+
+    outputs = layers.Conv2D(filters=48, kernel_size=[10, 4], strides=[2, 1])(outputs)
+    outputs = layers.BatchNormalization()(outputs)
+    outputs = layers.ReLU()(outputs)
+    outputs = layers.Dropout(0.1)(outputs)
+
+    outputs = layers.Flatten()(outputs)
+
+    outputs = layers.Dense(16)(outputs)
+    outputs = layers.BatchNormalization()(outputs)
+    outputs = layers.ReLU()(outputs)
+    outputs = layers.Dropout(0.1)(outputs)
+
+    outputs = layers.Dense(128, activation="relu")(outputs)
+    outputs = layers.Dense(12, activation="softmax")(outputs)
+
+    return keras.Model(inputs, outputs)
+
+
+def dscnn():
+    inputs = keras.Input(shape=(49, 10))
+    outputs = tf.expand_dims(inputs, axis=-1)
+
+    outputs = layers.Conv2D(
+        filters=172, kernel_size=[10, 4], strides=[2, 2], padding="same"
+    )(outputs)
+    outputs = layers.BatchNormalization()(outputs)
+    outputs = layers.ReLU()(outputs)
+    outputs = layers.Dropout(0.1)(outputs)
+
+    # DepthwiseConv2D 1
+    outputs = layers.DepthwiseConv2D(
+        kernel_size=[3, 3], strides=[1, 1], padding="same"
+    )(outputs)
+    outputs = layers.Conv2D(
+        filters=172, kernel_size=[1, 1], strides=[1, 1], padding="same"
+    )(outputs)
+    outputs = layers.BatchNormalization()(outputs)
+    outputs = layers.ReLU()(outputs)
+    outputs = layers.Dropout(0.1)(outputs)
+
+    # DepthwiseConv2D 2
+    outputs = layers.DepthwiseConv2D(
+        kernel_size=[3, 3], strides=[1, 1], padding="same"
+    )(outputs)
+    outputs = layers.Conv2D(
+        filters=172, kernel_size=[1, 1], strides=[1, 1], padding="same"
+    )(outputs)
+    outputs = layers.BatchNormalization()(outputs)
+    outputs = layers.ReLU()(outputs)
+    outputs = layers.Dropout(0.1)(outputs)
+
+    # DepthwiseConv2D 3
+    outputs = layers.DepthwiseConv2D(
+        kernel_size=[3, 3], strides=[1, 1], padding="same"
+    )(outputs)
+    outputs = layers.Conv2D(
+        filters=172, kernel_size=[1, 1], strides=[1, 1], padding="same"
+    )(outputs)
+    outputs = layers.BatchNormalization()(outputs)
+    outputs = layers.ReLU()(outputs)
+    outputs = layers.Dropout(0.1)(outputs)
+
+    # DepthwiseConv2D 4
+    outputs = layers.DepthwiseConv2D(
+        kernel_size=[3, 3], strides=[1, 1], padding="same"
+    )(outputs)
+    outputs = layers.Conv2D(
+        filters=172, kernel_size=[1, 1], strides=[1, 1], padding="same"
+    )(outputs)
+    outputs = layers.BatchNormalization()(outputs)
+    outputs = layers.ReLU()(outputs)
+    outputs = layers.Dropout(0.1)(outputs)
+
+    outputs = layers.AveragePooling2D(
+        pool_size=outputs.shape[1:3], strides=[2, 2], padding="valid"
+    )(outputs)
+
+    outputs = layers.Flatten()(outputs)
+    outputs = layers.Dense(12, activation="softmax")(outputs)
+
+    return keras.Model(inputs, outputs)
--- kws/train.py
+++ kws/train.py
@@ -6,10 +6,10 @@ import numpy as np
 import os
 
 from tensorflow import keras
-from tensorflow.keras.datasets import mnist
 from tensorflow.keras import layers, losses, metrics, optimizers, models
 
 from config import *
+from models import *
 
 x_train = np.load("./temp/train_x.npy")
 y_train = np.load("./temp/train_y.npy")
@@ -21,48 +21,16 @@ MODEL_PATH = "./temp/my"
 if os.path.exists(MODEL_PATH):
     model = keras.models.load_model(MODEL_PATH)
 else:
-    inputs = keras.Input(shape=(49, 10))
-    outputs = tf.expand_dims(inputs, axis=-1)
-
-    outputs = layers.Conv2D(filters=64, kernel_size=[10, 4], strides=[1, 1])(outputs)
-    outputs = layers.BatchNormalization()(outputs)
-    outputs = layers.ReLU()(outputs)
-    outputs = layers.Dropout(0.1)(outputs)
-
-    outputs = layers.Conv2D(filters=48, kernel_size=[10, 4], strides=[2, 1])(outputs)
-    outputs = layers.BatchNormalization()(outputs)
-    outputs = layers.ReLU()(outputs)
-    outputs = layers.Dropout(0.1)(outputs)
-
-    outputs = layers.Flatten()(outputs)
-
-    outputs = layers.Dense(16)(outputs)
-    outputs = layers.BatchNormalization()(outputs)
-    outputs = layers.ReLU()(outputs)
-    outputs = layers.Dropout(0.1)(outputs)
-
-    outputs = layers.Dense(128, activation="relu")(outputs)
-    outputs = layers.Dense(12, activation="softmax")(outputs)
-
-    model = keras.Model(inputs, outputs)
+    model = dscnn()
 
 model.compile(
     optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
 )
-model.fit(x_train, y_train, batch_size=200, epochs=20, shuffle=True, verbose=1)
+model.fit(x_train, y_train, batch_size=100, epochs=20, shuffle=True, verbose=1)
 
 x_test = np.load("./temp/test_x.npy")
 y_test = np.load("./temp/test_y.npy")
 x_test = np.squeeze(x_test)
-
-print("evalute for all")
-model.evaluate(x_test, y_test, verbose=2)
-for w in WORDS_TO_CHECK:
-    x = np.load("./temp/test_" + w + "_x.npy")
-    y = np.load("./temp/test_" + w + "_y.npy")
-    x = np.squeeze(x)
-    print("evalute for " + w + "")
-    model.evaluate(x, y, verbose=2)
 
 model.save(MODEL_PATH)
 
@@ -73,3 +41,12 @@ tflite = converter.convert()
 with open("./temp/output.tflite", "wb") as f:
     f.write(tflite)
     print("size of tflite:", len(tflite))
+
+print("evalute for all")
+model.evaluate(x_test, y_test, verbose=2)
+for w in WORDS_TO_CHECK:
+    x = np.load("./temp/test_" + w + "_x.npy")
+    y = np.load("./temp/test_" + w + "_y.npy")
+    x = np.squeeze(x)
+    print("evalute for " + w + "")
+    model.evaluate(x, y, verbose=2)
