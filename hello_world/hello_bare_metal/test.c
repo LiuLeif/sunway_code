e3f22a5383f034383e906f267e9c3a84f40887b9
sunway <sunwayforever@gmail.com>
2020-09-29 16:38:38 +0800

    nil

--- hello_world/hello_bare_metal/test.c
+++ hello_world/hello_bare_metal/test.c
@@ -1,4 +1,5 @@
 // 2020-09-29 11:56
+#include <stdlib.h>
 volatile unsigned int *const UART0DR = (unsigned int *)0x101f1000;
 
 void print_uart0(const char *s) {
@@ -8,4 +9,10 @@ void print_uart0(const char *s) {
     }
 }
 
-void c_entry() { print_uart0("Hello world!\n"); }
+void main() {
+    char *buff = (char*)malloc(10);
+    memset(buff, 0, 10);
+    print_uart0("Hello world!\n");
+    sprintf(buff, "hello");
+    print_uart0(buff);
+}
