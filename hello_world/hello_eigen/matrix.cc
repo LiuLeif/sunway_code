7bd03c6001f606d5d004dd99f87e65ddb26e8ffb
sunway <sunwayforever@gmail.com>
2021-01-02 19:17:38 +0800

    nil

--- hello_world/hello_eigen/matrix.cc
+++ hello_world/hello_eigen/matrix.cc
@@ -3,11 +3,13 @@
 
 using Eigen::MatrixXd;
 
-int main() {
+void test_matrix() {
     MatrixXd m(2, 2);
     m(0, 0) = 3;
+    
     m(1, 0) = 2.5;
     m(0, 1) = -1;
     m(1, 1) = m(1, 0) + m(0, 1);
     std::cout << m << std::endl;
 }
+int main() { test_matrix(); }
