9f9b73e5b4d38cf3c62892eac3a6c7ad3461e0ac
sunway <wei.sun@hexintek.com>
2021-12-20 15:18:57 +0800

    nil

---
+++ hello_world/hello_sycl/2.hello_usm.cu
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+__global__ void VecAdd(float4* a, float4* b, float4* c) {
+    c[0].x = a[0].x + b[0].x;
+    c[0].y = a[0].y + b[0].y;
+    c[0].z = a[0].z + b[0].z;
+    c[0].w = a[0].w + b[0].w;
+}
+
+void hello_usm() {
+    float4 a = {1.0, 2.0, 3.0, 4.0};
+    float4 b = {1.0, 2.0, 3.0, 4.0};
+    float4 c = {0.0, 0.0, 0.0, 0.0};
+
+    float4 *d_a, *d_b, *d_c;
+    cudaMalloc(&d_a, sizeof(float4));
+    cudaMalloc(&d_b, sizeof(float4));
+    cudaMalloc(&d_c, sizeof(float4));
+
+    cudaMemcpy(d_a, &a, sizeof(float4), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, &b, sizeof(float4), cudaMemcpyHostToDevice);
+
+    VecAdd<<<1, 1>>>(d_a, d_b, d_c);
+
+    cudaDeviceSynchronize();
+
+    cudaMemcpy(&c, d_c, sizeof(float4), cudaMemcpyDeviceToHost);
+    printf("%f %f %f %f\n", c.x, c.y, c.z, c.w);
+}
+
+void hello_usm_shared() {
+    float4 a = {1.0, 2.0, 3.0, 4.0};
+    float4 b = {1.0, 2.0, 3.0, 4.0};
+
+    float4 *d_a, *d_b, *c;
+    cudaMalloc(&d_a, sizeof(float4));
+    cudaMalloc(&d_b, sizeof(float4));
+    cudaMallocManaged(&c, sizeof(float4));
+
+    cudaMemcpy(d_a, &a, sizeof(float4), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, &b, sizeof(float4), cudaMemcpyHostToDevice);
+
+    VecAdd<<<1, 1>>>(d_a, d_b, c);
+
+    cudaDeviceSynchronize();
+
+    printf("%f %f %f %f\n", c->x, c->y, c->z, c->w);
+}
+
+int main(int argc, char* argv[]) {
+    hello_usm();
+    hello_usm_shared();
+    return 0;
+}
