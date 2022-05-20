d0cd65cecf7b11f6a3e46e287f63e72fdc6025bb
sunway <wei.sun@hexintek.com>
2022-05-20 18:19:45 +0800

    nil

--- hello_world/hello_go/src/hexintek.com/monitor/router.go
+++ hello_world/hello_go/src/hexintek.com/monitor/router.go
@@ -6,10 +6,10 @@ import "github.com/gin-gonic/gin"
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("src/hexintek.com/monitor/ui/*")
-	router.Static("/assets", "src/hexintek.com/monitor/assets")
-
+    
 	router.GET("/devices/", handleGetDevices)
 	router.POST("/devices/", handleInsertDevice)
-	router.POST("/upload/", handleUpload)	
+	router.POST("/upload/", handleUpload)
+	
 	router.Run()
 }
