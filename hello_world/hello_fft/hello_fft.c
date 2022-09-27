e27478bd16e59999120c6cf932213d0ec324f5d9
sunway <wei.sun@hexintek.com>
2022-09-27 19:21:34 +0800

    nil

--- hello_world/hello_fft/hello_fft.c
+++ hello_world/hello_fft/hello_fft.c
@@ -10,6 +10,31 @@
 #define PI 3.14159
 #define N 128
 
+void my_fft(int n_point, kiss_fft_cpx *in, kiss_fft_cpx *out) {
+    for (int i = 0; i < n_point; i++) {
+        float tmp_r = 0.0f;
+        float tmp_i = 0.0f;
+        for (int j = 0; j < n_point; j++) {
+            tmp_r += in[j].r * cos(i * 2.0 * PI * j / n_point);
+            tmp_i -= in[j].r * sin(i * 2.0 * PI * j / n_point);
+        }
+        out[i].r = tmp_r;
+        out[i].i = tmp_i;
+    }
+}
+
+void my_ifft(int n_point, kiss_fft_cpx *in, kiss_fft_cpx *out) {
+    for (int i = 0; i < n_point; i++) {
+        float tmp = 0.0f;
+        for (int j = 0; j < n_point; j++) {
+            tmp += in[j].r * cos(2.0 * PI * i * j / n_point);
+            tmp -= in[j].i * sin(2.0 * PI * i * j / n_point);
+        }
+        out[i].r = tmp;
+        out[i].i = 0.0f;
+    }
+}
+
 int main(int argc, char *argv[]) {
     kiss_fft_cfg cfg = kiss_fft_alloc(N, 0, 0, 0);
     kiss_fft_cpx *in =
@@ -22,25 +47,18 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < N; i++) {
         in[i].r = cos(i * 2.0 * PI / N) + 10 * sin(2 * i * 2.0 * PI / N);
     }
-    printf("------\n");
-    printf("using kissfft:\n");
+    printf("fft:\n");
     kiss_fft(cfg, in, out);
     printf("1hz cos: %f\n", out[1].r / 64);
     printf("2hz sin: %f\n", -out[2].i / 64);
+    printf("------\n");
 
+    printf("fft manually:\n");
+    my_fft(N, in, out);
+    printf("1hz cos: %f\n", out[1].r / 64);
+    printf("2hz sin: %f\n", -out[2].i / 64);
     printf("------\n");
-    printf("calc manually:\n");
-    float a = 0.0;
-    for (int i = 0; i < N; i++) {
-        a += in[i].r * cos(2 * PI * i / N);
-    }
-    printf("1hz cos: %f\n", a / 64);
-    float b = 0.0;
-    for (int i = 0; i < N; i++) {
-        b += in[i].r * sin(2.0 * PI * 2.0 * i / N);
-    }
-    printf("2hz sin: %f\n", b / 64);
-    printf("------\n");
+
     printf("ifft:\n");
     cfg = kiss_fft_alloc(N, 1, 0, 0);
     kiss_fft(cfg, out, in_restored);
@@ -48,13 +66,10 @@ int main(int argc, char *argv[]) {
         printf("%f %f\n", in_restored[i].r / N, in[i].r);
     }
     printf("------\n");
+
     printf("ifft manually:\n");
+    my_ifft(N, out, in_restored);
     for (int i = 0; i < 10; i++) {
-        float x_restored = 0;
-        for (int k = 0; k < N; k++) {
-            x_restored += out[k].r * cos(2.0 * PI * i * k / N);
-            x_restored -= out[k].i * sin(2.0 * PI * i * k / N);
-        }
-        printf("%f %f\n", x_restored / N, in[i].r);
+        printf("%f %f\n", in_restored[i].r / N, in[i].r);
     }
 }
