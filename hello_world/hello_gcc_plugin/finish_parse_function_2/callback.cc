5cd9461263d195994136b7c1a4ef3d92f18c0cf9
sunway <wei.sun@hexintek.com>
2023-04-06 15:53:22 +0800

    nil

--- hello_world/hello_gcc_plugin/finish_parse_function_2/callback.cc
+++ hello_world/hello_gcc_plugin/finish_parse_function_2/callback.cc
@@ -7,13 +7,34 @@
 #include <tree-iterator.h>
 #include <tree-pretty-print.h>
 #include <iostream>
+#include <stringpool.h>
+#include "plugin.h"
+
 // clang-format on
+static tree handle_notrace_attribute(
+    tree *node, tree name, tree args, int flags, bool *no_add_attrs) {
+    return NULL_TREE;
+}
+
+static struct attribute_spec notrace_attr = {
+    "notrace", 0, 0, false, false, false, false, handle_notrace_attribute};
+
+void callback_register_attribute(void *data, void *__unused__) {
+    register_attribute(&notrace_attr);
+}
 
 void callback_parse_function(void *data, void *__unused__) {
     std::cerr << "======" << std::endl;
     std::cerr << "FUNCTION: " << std::endl;
     tree t = (tree)data;
-    // debug_tree(t);
+    tree attributes = DECL_ATTRIBUTES(t);
+    if (attributes) {
+        for (tree t = attributes; t != NULL_TREE; t = TREE_CHAIN(t)) {
+            if (TREE_PURPOSE(t) == get_identifier("notrace")) {
+                return;
+            }
+        }
+    }
     const char *fname = IDENTIFIER_POINTER(DECL_NAME(t));
     if (strcmp(fname, "trace") == 0) {
         return;
@@ -42,4 +63,6 @@ void callback_parse_function(void *data, void *__unused__) {
 void register_callbacks(const char *base_name) {
     register_callback(
         base_name, PLUGIN_FINISH_PARSE_FUNCTION, callback_parse_function, NULL);
+    register_callback(
+        base_name, PLUGIN_ATTRIBUTES, callback_register_attribute, NULL);
 }
--- hello_world/hello_gcc_plugin/finish_parse_function_2/test.c
+++ hello_world/hello_gcc_plugin/finish_parse_function_2/test.c
@@ -1,6 +1,7 @@
 // 2023-03-27 14:24
 #include <stdio.h>
-void foo(){};
+void bar(){};
+__attribute__((notrace)) void foo() { bar(); };
 void trace(char *func) { printf("---%s---\n", func); }
 int main(int argc, char *argv[]) {
     foo();
