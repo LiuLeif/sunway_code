6ec27dc777e0ad7238b588e8fe37d25e37748881
sunway <wei.sun@hexintek.com>
2023-04-19 18:24:47 +0800

    nil

--- hello_world/hello_neon/neon_emu/vld.h
+++ hello_world/hello_neon/neon_emu/vld.h
@@ -3,76 +3,40 @@
 #define VLD_H
 #include "neon_emu_types.h"
 
-int8x8_t vld1_s8(const int8_t* ptr) {
-    int8x8_t r;
-    memcpy(&r, ptr, sizeof(r));
-    return r;
-}
+#define DEF_VLD1(base, sign, size, n)                                   \
+    base##size##x##n##_t vld1_##sign##size(const base##size##_t* ptr) { \
+        base##size##x##n##_t r;                                         \
+        memcpy(&r, ptr, sizeof(r));                                     \
+        return r;                                                       \
+    }
 
-int16x4_t vld1_s16(const int16_t* ptr) {
-    int16x4_t r;
-    memcpy(&r, ptr, sizeof(r));
-    return r;
-}
+#define DEF_VLD1Q(base, sign, size, n)                                   \
+    base##size##x##n##_t vld1q_##sign##size(const base##size##_t* ptr) { \
+        base##size##x##n##_t r;                                          \
+        memcpy(&r, ptr, sizeof(r));                                      \
+        return r;                                                        \
+    }
 
-uint8x8_t vld1_u8(const uint8_t* ptr) {
-    uint8x8_t r;
-    memcpy(&r, ptr, sizeof(r));
-    return r;
-}
+DEF_VLD1(int, s, 8, 8);
+DEF_VLD1(uint, u, 8, 8);
+DEF_VLD1(int, s, 16, 4);
+DEF_VLD1(uint, u, 16, 4);
+DEF_VLD1(int, s, 32, 2);
+DEF_VLD1(uint, u, 32, 2);
+DEF_VLD1(int, s, 64, 1);
+DEF_VLD1(uint, u, 64, 1);
+DEF_VLD1(float, f, 32, 2);
+DEF_VLD1(float, f, 64, 1);
 
-uint16x4_t vld1_u16(const uint16_t* ptr) {
-    uint16x4_t r;
-    memcpy(&r, ptr, sizeof(r));
-    return r;
-}
-
-uint32x2_t vld1_u32(const uint32_t* ptr) {
-    uint32x2_t r;
-    memcpy(&r, ptr, sizeof(r));
-    return r;
-}
-
-uint64x1_t vld1_u64(const uint64_t* ptr) {
-    uint64x1_t r;
-    memcpy(&r, ptr, sizeof(r));
-    return r;
-}
-
-int64x1_t vld1_s64(const int64_t* ptr) {
-    int64x1_t r;
-    memcpy(&r, ptr, sizeof(r));
-    return r;
-}
-
-int8x16_t vld1q_s8(const int8_t* ptr) {
-    int8x16_t r;
-    memcpy(&r, ptr, sizeof(r));
-    return r;
-}
-
-int16x8_t vld1q_s16(const int16_t* ptr) {
-    int16x8_t r;
-    memcpy(&r, ptr, sizeof(r));
-    return r;
-}
-
-int32x4_t vld1q_s32(const int32_t* ptr) {
-    int32x4_t r;
-    memcpy(&r, ptr, sizeof(r));
-    return r;
-}
-
-float32x2_t vld1_f32(const float* ptr) {
-    float32x2_t r;
-    memcpy(&r, ptr, sizeof(r));
-    return r;
-}
-
-float32x4_t vld1q_f32(const float* ptr) {
-    float32x4_t r;
-    memcpy(&r, ptr, sizeof(r));
-    return r;
-}
+DEF_VLD1Q(int, s, 8, 16);
+DEF_VLD1Q(uint, u, 8, 16);
+DEF_VLD1Q(int, s, 16, 8);
+DEF_VLD1Q(uint, u, 16, 8);
+DEF_VLD1Q(int, s, 32, 4);
+DEF_VLD1Q(uint, u, 32, 4);
+DEF_VLD1Q(int, s, 64, 2);
+DEF_VLD1Q(uint, u, 64, 2);
+DEF_VLD1Q(float, f, 32, 4);
+DEF_VLD1Q(float, f, 64, 2);
 
 #endif  // VLD_H
--- hello_world/hello_neon/neon_emu/vshr.h
+++ hello_world/hello_neon/neon_emu/vshr.h
@@ -19,4 +19,28 @@ uint16x4_t vshr_n_u16(uint16x4_t a, int n) {
     }
     return r;
 }
+
+int8x8_t vrshr_n_s8(int8x8_t a, int n) {
+    int8x8_t r;
+    for (int i = 0; i < 8; i++) {
+        r.values[i] = (a.values[i] + (1 << (n - 1))) >> n;
+    }
+    return r;
+}
+
+int8x8_t vsra_n_s8(int8x8_t a, int8x8_t b, int n) {
+    int8x8_t r;
+    for (int i = 0; i < 8; i++) {
+        r.values[i] = a.values[i] + (b.values[i] >> n);
+    }
+    return r;
+}
+
+int8x8_t vrsra_n_s8(int8x8_t a, int8x8_t b, int n) {
+    int8x8_t r;
+    for (int i = 0; i < 8; i++) {
+        r.values[i] = a.values[i] + ((b.values[i] + (1 << (n - 1))) >> n);
+    }
+    return r;
+}
 #endif  // VSHR_H
